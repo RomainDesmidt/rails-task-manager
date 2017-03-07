c600ba6ffe2d2a98f8b26cf98add86850fd31441
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-07 16:31:11 +0100

    ruby on rails intensifies

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'tasks', to: "tasks#index"
-  get 'tasks/:id', to: 'tasks#show'
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post 'tasks', to: 'tasks#create', as: 'create_task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
